2235615a836d73b188084b0d2445b60363d6c97c
gigi603 <gilbert.trinidad1@gmail.com>
2022-03-23 00:29:47 +0100

    padding title responsive in project detail

--- src/views/ProjectPage.vue
+++ src/views/ProjectPage.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-wrap px-4">
       <div v-for="(project, index) in projects" :key="index" class="relative flex flex-col min-w-0 break-words bg-white w-full shadow-lg rounded-3xl">
         <div v-if="project.id == $route.params.id ">
-          <div class="pt-10" style="padding-left: 4.5rem;"><h2 class="font-bold text-2xl"><button @click="$router.go(-1)" class="mr-2"><font-awesome-icon icon="fa-solid fa-chevron-left" color="#000" size="sm" /></button> What is {{ project.name }} ?</h2></div>
+          <div class="pt-10 pl-4 md:pl-20"><h2 class="font-bold text-2xl"><button @click="$router.go(-1)" class="mr-2"><font-awesome-icon icon="fa-solid fa-chevron-left" color="#000" size="sm" /></button> What is {{ project.name }} ?</h2></div>
           <div class="md:px-16 py-10 grid grid-cols-1 flex-auto rounded-3xl">
             <p class="px-6 md:px-16">{{ project.description }} <a href="https://app.unfate.co/" class="font-bold text-black" >Unfate</a>.</p>
             <div class="py-10 grid sm:grid-cols-1 md:grid-cols-4 flex-auto ">
